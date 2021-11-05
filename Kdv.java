e3123e67ffbfd069e90d4c8b10cc034fad46d7be
abdullahcetin09 <09abdullahcetin@gmail.com>
2021-11-05 15:08:03 +0300

    Kdv hesaplama

---
+++ Kdv.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Kdv {
+    public static void main(String[] args) {
+        double deger ,kdvli,kdvorani,kdv ;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("Bir tutar giriniz ?");
+        deger=inp.nextFloat();
+
+        if(deger <=1000){kdvorani= 0.18;}
+        else{kdvorani=0.08;}
+        kdv=deger*kdvorani;
+        kdvli=deger+kdv;
+    
+        System.out.println("Girdiğiniz tutara ait KDV oranı : " + kdvorani +"\nKDV tutarı : "+ kdv+"\nKDV'li tutar : "+kdvli+"\nKDV'siz tutar : "+deger);
+      
+
+
+
+
+
+
+
+
+    }
+
+
+}
