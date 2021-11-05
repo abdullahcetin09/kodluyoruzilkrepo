52be16c230dfa005f4adb539e5f966325711afeb
abdullahcetin09 <09abdullahcetin@gmail.com>
2021-11-05 21:09:50 +0300

    Vücud kitle endeksi eklenmiş

---
+++ VucudKitleIndeksi.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class VucudKitleIndeksi {
+    public static void main(String[] args) {
+         
+        float boy , kilo;
+
+        Scanner in=new Scanner(System.in);
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz :");
+        boy=in.nextFloat();
+        System.out.print("\nLütfen kilonuzu giriniz :");
+        kilo=in.nextFloat();
+        System.out.print("\nVücut Kitle İndeksiniz :"+ (kilo/(boy*boy)));
+    }
+    
+}
